acaf68a28b23faedf634623c2d9dfd1c24aa466a
Michael <mbahl1670@gmail.com>
2022-01-19 20:38:57 -0600

    working on comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
-const {} = require('../../models');
+const { Comment } = require('../../models');
 
+// GET ALL comments route /api/comments
+router.get('/', (req,res) => {});
 
+// GET a SINGLE comment route /api/comments/:id
+router.get('/:id', (req,res) => {});
+
+// POST create a Comment route /api/comments
+router.post('/', (req,res) => {});
+
+// PUT change Comment info route /api/comments/:id
+router.put('/:id', (req, res) => {});
+
+// DELETE a Comment route /api/comments/:id
+router.delete('/:id', (req, res) => {});
 
 module.exports = router;
--- controllers/api/park-routes.js
+++ controllers/api/park-routes.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
-const {} = require('../../models');
+const { Park } = require('../../models');
 
+// GET ALL parks route /api/parks
+router.get('/', (req,res) => {});
 
+// GET a SINGLE park route /api/parks/:id
+router.get('/:id', (req,res) => {});
+
+// POST create a Park route /api/parks
+router.post('/', (req,res) => {});
+
+// PUT change Park info route /api/parks/:id
+router.put('/:id', (req, res) => {});
+
+// DELETE a Park route /api/parks/:id
+router.delete('/:id', (req, res) => {});
 
 module.exports = router;
