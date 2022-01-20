cba90fb6ac4f4f12dc1b767558dc4ac1ca3e8cc5
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-01-19 19:16:39 -0600

    Add require Path,hbs and set Port

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const path = require('path');
+
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, './public')));
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(PORT, () => console.log('Now listening'));
