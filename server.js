b19d4de72a0a9f1d86d803294ca3458f5ef87fda
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-01-21 08:26:55 -0600

    modified server.js

--- server.js
+++ server.js
@@ -7,15 +7,13 @@ const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(express.static(path.join(__dirname, './public')));
-
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, './public')));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.listen(PORT, () => console.log('Now listening'));
