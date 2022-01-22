eb0f169a9bbd05e61c5b9c92bc4a5ea761819050
Christopher Speltz <chrisspeltz@msn.com>
2022-01-22 10:43:27 -0600

    updatd park model

--- models/Park.js
+++ models/Park.js
@@ -55,13 +55,6 @@ Park.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "user",
-        key: "id",
-      },
-    },
   },
   {
     sequelize,
